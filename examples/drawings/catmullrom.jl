874773bb65a99e6d5f63bb7dc6fba6d46e3ccd5e
WooKyoung Noh <wookay.noh@gmail.com>
2019-08-01 16:44:46 +0900

    fix transform

---
+++ examples/drawings/catmullrom.jl
@@ -0,0 +1,36 @@
+# [deps]
+# CatmullRom.jl
+#     https://github.com/JeffreySarnoff/CatmullRom.jl
+
+using Poptart.Desktop # Application Windows put!
+using Poptart.Controls # Canvas
+using Poptart.Drawings # Polyline stroke
+using Colors: RGBA
+using CatmullRom # catmullrom
+
+canvas = Canvas()
+window1 = Windows.Window(items=[canvas], title="Drawings", frame=(x=0, y=0, width=550, height=400))
+closenotify = Condition()
+Application(windows=[window1], title="App", frame=(width=550, height=400), closenotify=closenotify)
+
+strokeColor = RGBA(0, 0.7, 0, 1)
+romColor    = RGBA(0.8, 0.8, 0.2, 1)
+
+fx(t) = cospi(t)
+fy(t) = sinpi(t)
+xs = [fx(t) for t=range(0.0, stop=2.0, length=17)]
+ys = [fy(t) for t=range(0.0, stop=2.0, length=17)]
+points = collect(zip(xs, ys))
+
+cxs, cys = catmullrom(points, 16)
+rom_points = collect(zip(cxs, cys))
+
+transform(point) = translate(scale(point, 200), 100)
+
+polyline1 = Polyline(points=transform.(points), color=strokeColor, thickness=9)
+put!(canvas, stroke(polyline1))
+
+polyline2 = Polyline(points=transform.(rom_points), color=romColor, thickness=9)
+put!(canvas, stroke(polyline2))
+
+Base.JLOptions().isinteractive==0 && wait(closenotify)
--- examples/graphs/digraph.jl
+++ examples/graphs/digraph.jl
@@ -20,24 +20,24 @@ using Colors: RGBA
 using Compose: inch
 
 canvas = Canvas()
-transform(element) = translate(scale(element, 5inch.value), 200)
+transform(point) = translate(scale(point, 5inch.value), 200)
 
 textColor = RGBA(0.8, 0.7, 0.8, 0.9)
 for (i, n) in enumerate(plot.nodes)
     rect = ((n.center .- n.radius)..., (n.center .+ n.radius)...)
     node = nodes[i]
-    textbox = TextBox(text=String(node.id), rect=rect, color=textColor) |> transform
+    textbox = TextBox(text=String(node.id), rect=transform(rect), color=textColor)
     put!(canvas, textbox)
 end
 
 thickness = 6
 strokeColor = RGBA(0.1, 0.7, 0.8, 1)
 for points in plot.lines
-    line = Line(points=points, thickness=thickness, color=strokeColor) |> transform
+    line = Line(points=transform.(points), thickness=thickness, color=strokeColor)
     put!(canvas, stroke(line))
 end
 for points in plot.arrows
-    arrow = Polyline(points=points, thickness=thickness, color=strokeColor) |> transform
+    arrow = Polyline(points=transform.(points), thickness=thickness, color=strokeColor)
     put!(canvas, stroke(arrow))
 end
 
